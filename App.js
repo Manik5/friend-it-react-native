4431ba63c53766133a06eee14d50025351bfc1c9
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-06-17 17:44:16 +0200

    set up homescreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import flatsReducer from './redux/reducers/flat.reducer';
+
+import FlatNavigator from './navigation/FlatNavigator';
 
 const rootReducer = combineReducers({
   flats: flatsReducer
@@ -15,7 +16,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
      <Provider store={store}>
-       <View></View>
+       <FlatNavigator />
      </Provider>
   );
 }
---
+++ navigation/FlatNavigator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Platform } from 'react-native';
+
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createDrawerNavigator, DrawerNavigatorItems } from 'react-navigation-drawer';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '../constants/color.constant';
+
+import HomeScreen from '../screens/HomeScreen';
+
+
+const FlatNavigator = createStackNavigator({
+  Home: HomeScreen
+}, {
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: 'white'
+    },
+    headerTintColor: Colors.primary
+  }
+});
+
+export default createAppContainer(FlatNavigator);
