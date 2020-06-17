04cfef6413624f2e93610eb5ba0cc6f0b44dae34
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-06-17 17:04:42 +0200

    imported navigation dependencies

---
+++ navigation/Navigator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { BottomNavigation } from 'react-native-material-ui';
+
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createDrawerNavigator, DrawerNavigatorItems } from 'react-navigation-drawer';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+import { Ionicons } from '@expo/vector-icons';
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Text, View, StyleSheet, FlatList } from 'react-native';
 
+import { useSelector } from 'react-redux';
+
 const HomeScreen = props => {
+  const flats = useSelector(state => state.flats.availableFlats);
   return (
-    <View>
-      <Text>
-        This is the HomeScreen!
-      </Text>
-    </View>
+   <FlatList
+    data={flats}
+    keyExtractor={item => item.id}
+    renderItem={
+      itemData =>
+        <Text>
+          {itemData.item.title}
+        </Text>
+      }
+   />
   )
 }
 
