76c2ff0fa357d67a91bf43bbef41379bba0dbeb7
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-06-17 20:45:28 +0200

    home screen

--- components/FlatCard.js
+++ components/FlatCard.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
+import { Card, Button } from 'react-native-elements';
 
 const FlatCard = props => {
-	return (
-		<View>
+  return (
+    <Card>
+      <Image
+        style={styles.image}
+        source={{ uri: props.image}}
+      />
+      <Text style={styles.title}>{props.title}</Text>
+      <Text style={styles.price}>${props.price}</Text>
+      <View style={styles.button}>
+        <Button
+          title="View Details"
+          onPress={props.onViewDetail}
+        />
+      </View>
+    </Card>
+  );
+};
 
-		</View>
-	)
-}
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 200
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4
+  },
+  price: {
+    fontSize: 14,
+    color: '#888'
+  },
+  button: {
+    borderRadius: 20
+  }
+})
+
+export default FlatCard;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { Text, View, StyleSheet, FlatList } from 'react-native';
 
 import { useSelector } from 'react-redux';
 
+import FlatCard from '../components/FlatCard';
+
 
 const HomeScreen = props => {
   const flats = useSelector(state => state.flats.availableFlats);
@@ -12,9 +14,11 @@ const HomeScreen = props => {
        keyExtractor={item => item.id}
        renderItem={
          itemData =>
-           <Text>
-             {itemData.item.title}
-           </Text>
+          <FlatCard
+            image={itemData.item.imageUrl}
+            title={itemData.item.title}
+            price={itemData.item.price}
+          />
          }
       />
   )
